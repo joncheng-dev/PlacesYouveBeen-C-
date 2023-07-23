b2d30f6e290dd1f8923ba3be42b4d5cf06282932
Jonathan Cheng <jcheng461@gmail.com>
2023-07-22 23:18:03 -0700

    WIP: update UI for user to submit new entries, and display them properly

--- Places/Controllers/PlacesController.cs
+++ Places/Controllers/PlacesController.cs
@@ -6,27 +6,24 @@ namespace Places.Controllers
 {
   public class PlacesController : Controller
   {
-    // [HttpGet("/journal")]
-    // public ActionResult Index()
-    // {
-
-    //   List<Item> allPackages = Item.GetAll();
-    //   return View(allPackages);
-    // }
+    [HttpGet("/journal")]
+    public ActionResult Index()
+    {
+      List<Place> allEntries = Place.GetAll();
+      return View(allEntries);
+    }
 
     [HttpGet("/journal/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
 
-    // [HttpPost("/journal")]
-    // public ActionResult Create(double weight, double length, double width, double height)
-    // {
-    //   Item anItem = new Item(weight, length, width, height);
-    //   anItem.CalculateVolume();
-    //   anItem.CalculateCost();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost("/journal")]
+    public ActionResult Create(string cityName, int lengthOfStay, string journalEntry, List<string> companionList, List<string> pictureList)
+    {
+      Place aNewPlace = new Place(cityName, lengthOfStay, journalEntry, companionList, pictureList);
+      return RedirectToAction("Index");
+    }
   }
 }
